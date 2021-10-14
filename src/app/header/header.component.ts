0f7055f127dcf6f081b781d3c93a3f909cb6ba1d
Ronel Andaya <ronel.andaya@gmail.com>
2021-10-15 02:49:03 +0800

    Storing Recipes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
 
 
 @Component({
@@ -7,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  constructor() { }
+  constructor(private dataStorageService: DataStorageService) { }
+  onSaveData(){
+    this.dataStorageService.storeRecipes();
+  }
+  onFetchData(){
+    
+  }
 }
---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { RecipeService } from "../recipes/recipe.service";
+
+@Injectable({providedIn: 'root'})
+export class DataStorageService{
+    constructor(private http: HttpClient,
+                private recipeService: RecipeService){
+
+    }
+    storeRecipes(){
+        const recipes = this.recipeService.getRecipes();
+        return this.http.put('https://ng-course-recipe-book-5a1fd-default-rtdb.asia-southeast1.firebasedatabase.app/recipes.json',recipes)
+                            .subscribe(response =>{
+                                console.log(response);
+                            })
+    }
+    
+}
