cd0f2a36308ce8e7dbb2ac6ed346a9c2f5e7d886
Ronel Andaya <ronel.andaya@gmail.com>
2021-10-15 03:11:52 +0800

    Transforming Response Data

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -10,18 +10,19 @@ export class RecipeService{
    recipesChanged = new Subject<Recipe[]>();
   
     
-    private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 
-                   'This is simply a test',
-                   'https://www.seriouseats.com/thmb/EZaQnk1yjGYVIkASseEWqtFRHyc=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__serious_eats__seriouseats.com__recipes__images__2015__05__Anova-Steak-Guide-Sous-Vide-Photos15-beauty-159b7038c56a4e7685b57f478ca3e4c8.jpg',
-                   [new Ingredient('Burger', 5),
-                    new Ingredient('Meat', 10)]),
-        new Recipe('A Test Recipe 2',
-                   'This is simply a test 2',
-                   'https://images-gmi-pmc.edge-generalmills.com/b57ee35f-bce2-4229-8bf5-19b97876a4cb.jpg',
-                   [new Ingredient('Patti', 5),
-                    new Ingredient('Cheese', 10)])
-      ];
+   //  private recipes: Recipe[] = [
+   //      new Recipe('A Test Recipe', 
+   //                 'This is simply a test',
+   //                 'https://www.seriouseats.com/thmb/EZaQnk1yjGYVIkASseEWqtFRHyc=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__serious_eats__seriouseats.com__recipes__images__2015__05__Anova-Steak-Guide-Sous-Vide-Photos15-beauty-159b7038c56a4e7685b57f478ca3e4c8.jpg',
+   //                 [new Ingredient('Burger', 5),
+   //                  new Ingredient('Meat', 10)]),
+   //      new Recipe('A Test Recipe 2',
+   //                 'This is simply a test 2',
+   //                 'https://images-gmi-pmc.edge-generalmills.com/b57ee35f-bce2-4229-8bf5-19b97876a4cb.jpg',
+   //                 [new Ingredient('Patti', 5),
+   //                  new Ingredient('Cheese', 10)])
+   //    ];
+   private recipes: Recipe[] = [];
 constructor(private shoppingListService: ShoppingListService){
 
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { map } from "rxjs/operators";
+
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
 
@@ -18,6 +20,14 @@ export class DataStorageService{
     }
     fetchRecipes(){
         this.http.get<Recipe[]>('https://ng-course-recipe-book-5a1fd-default-rtdb.asia-southeast1.firebasedatabase.app/recipes.json')
+                       .pipe(map(recipes =>{
+                         return  recipes.map(recipe => {
+                             return {
+                                 ...recipe, 
+                                 ingredients: recipe.ingredients ? recipe.ingredients : []
+                             }
+                         })
+                       }))
                        .subscribe(recipes => {
                            this.recipeService.setRecipes(recipes);
                        });
