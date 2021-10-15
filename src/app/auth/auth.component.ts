2b00d804415a580ed976526c38cc56b11bede083
Ronel Andaya <ronel.andaya@gmail.com>
2021-10-16 01:55:25 +0800

    Reflecting the Auth State in the UI

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService, AuthResponseData } from './auth.service';
 
@@ -12,7 +13,8 @@ export class AuthComponent implements OnInit {
   isLoginMode = true;
   isLoading = false;
   error: string = null;
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService,
+              private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -20,34 +22,37 @@ export class AuthComponent implements OnInit {
     this.isLoginMode = !this.isLoginMode;
   }
   onSubmit(form: NgForm){
-    if(form.invalid){
-      return;
-    }
-    const email = form.value.email;
-    const password = form.value.password;
+      if(form.invalid){
+        return;
+      }
+      const email = form.value.email;
+      const password = form.value.password;
 
-    let authObs: Observable<AuthResponseData>;
+      let authObs: Observable<AuthResponseData>;
 
-    this.isLoading = true
-    if(this.isLoginMode){
-    authObs =  this.authService.login(email, password);
-        
-    }else{
-      authObs = this.authService.signUp(email, password);
-        
-   }
+      this.isLoading = true
+      if(this.isLoginMode){
+      authObs =  this.authService.login(email, password);
+          
+      }else{
+        authObs = this.authService.signUp(email, password);
+      
+      }
 
-   authObs.subscribe(resData => {
-    this.isLoading = false;
-    console.log(resData)
-  },errorMessage =>{
-    this.isLoading = false;
-    console.log(errorMessage);
-    this.error = errorMessage;
-  });
-     
-  form.reset();
-  
+    authObs.subscribe(resData => {
+      this.isLoading = false;
+      console.log(resData);
+      this.router.navigate(['/recipes']);
+
+    },errorMessage =>{
+      this.isLoading = false;
+      console.log(errorMessage);
+      this.error = errorMessage;
+
+    });
+      
+    form.reset();
+
   }
 
 }
