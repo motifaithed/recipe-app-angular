4d16279dd998386d6e33b689fdd12fc9118f2beb
Ronel Andaya <ronel.andaya@gmail.com>
2021-10-17 20:18:33 +0800

    Understanding the Core Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -9,16 +9,17 @@ import { HeaderComponent } from './header/header.component';
 
 import { AppRoutingModule } from './app-routing-module';
 
-import { RecipeService } from './recipes/recipe.service';
+
 import { AuthComponent } from './auth/auth.component';
 
-import { AuthInterceptor } from './auth/auth-interceptor.service';
 
 
-import { ShoppingListService } from './shopping-list/shopping-list.service';
+
+
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core.module';
 
 
 
@@ -38,14 +39,9 @@ import { SharedModule } from './shared/shared.module';
     HttpClientModule,
     RecipesModule,
     ShoppingListModule,
-    SharedModule
+    SharedModule,
+    CoreModule
   ],
-  providers: [ShoppingListService,
-              RecipeService, 
-              {provide: HTTP_INTERCEPTORS, 
-               useClass: AuthInterceptor, 
-               multi: true}
-              ],
   bootstrap: [AppComponent]
  
 })
