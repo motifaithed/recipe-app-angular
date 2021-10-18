4cc0fcd05ff6a685d987cbe484ec766924b03bc6
Ronel Andaya <ronel.andaya@gmail.com>
2021-10-19 01:26:47 +0800

    NGRX: Updating and Deleting Ingredients

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -43,7 +43,8 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     const value = form.value
     const newIngredient = new Ingredient(value.name, value.amount);
     if(this.editMode){
-      this.shoppingListService.updateIngredient(this.editedItemIndex, newIngredient);
+      //this.shoppingListService.updateIngredient(this.editedItemIndex, newIngredient);
+      this.store.dispatch(new ShoppingListActions.UpdateIngredient({index: this.editedItemIndex,ingredient: newIngredient}));
     }else{
       // this.shoppingListService.addIngredient(newIngredient);
       this.store.dispatch(new ShoppingListActions.AddIngredient(newIngredient));
@@ -54,8 +55,9 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   }
 
   onDelete(){
-    this.shoppingListService.deleteIngredient(this.editedItemIndex);
-    this.onClear();
+    // this.shoppingListService.deleteIngredient(this.editedItemIndex);
+    // this.onClear();
+    this.store.dispatch(new ShoppingListActions.DeleteIngredient(this.editedItemIndex));
   }
   onClear(){
 
