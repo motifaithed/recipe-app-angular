4321fbeeea53210e665322ca264eeeee31f6a48a
Ronel Andaya <ronel.andaya@gmail.com>
2021-10-19 02:40:22 +0800

    NGRX: Managing More State via NGRX

--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -27,12 +27,14 @@ export function shoppingListReducer(state: State = initialState,
                 ...state,
                 ingredients:[...state.ingredients, action.payload]
             };
+
         case ShoppingListActions.ADD_INGREDIENTS:
             return {
                 ...state,
                 ingredients:[...state.ingredients, ...action.payload]
 
             };
+
         case ShoppingListActions.UPDATE_INGREDIENT:
             const ingredient = state.ingredients[action.payload.index];
             const updatedIngredient = {
@@ -47,16 +49,29 @@ export function shoppingListReducer(state: State = initialState,
                 ...state,
                 ingredients: updatedIngredients
             };
+
         case ShoppingListActions.DELETE_INGREDIENT:
             return {
-                
-
                 ...state,
                 ingredients: state.ingredients.filter((ig, igIndex) =>{
 
                     return igIndex !== action.payload;
                 })
+            };
+
+        case ShoppingListActions.START_EDIT:
+            return {
+                ...state,
+                editedIngredientIndex: action.payload,
+                editedIngredient: {...state.ingredients[action.payload]}
             }
+        case ShoppingListActions.STOP_EDIT:
+            return{
+                ...state,
+                editedIngredient: null,
+                editedIngredientIndex: -1
+            };
+        
         default: 
             return state;
     }
