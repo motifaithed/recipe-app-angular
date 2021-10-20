80a2af61b19bcd263f3f905581222802219f29a4
Ronel Andaya <ronel.andaya@gmail.com>
2021-10-20 15:39:53 +0800

    NGRX:Fetching Recipe Detail Data

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit} from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
+import { map, switchMap } from 'rxjs/operators';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
+import { Store } from '@ngrx/store';
+import * as fromApp from '../../store/app.reducer'
+import * as RecipesActions from '../store/recipe.actions';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -15,18 +19,44 @@ export class RecipeDetailComponent implements OnInit {
   id: number;
   constructor(private recipeService: RecipeService,
               private route: ActivatedRoute,
-              private router: Router) { }
+              private router: Router,
+              private store: Store<fromApp.AppState>) { }
 
   ngOnInit(): void {
-      this.route.params
-        .subscribe(
-          (params: Params) => {
+      // this.route.params
+      //   .subscribe(
+      //     (params: Params) => {
 
-            this.id = +params['id'];
-            this.recipe = this.recipeService.getRecipe(this.id);
+      //       this.id = +params['id'];
+      //       //this.recipe = this.recipeService.getRecipe(this.id);
+      //       this.store.select('recipes').pipe(map(recipesState =>{
+      //         return recipesState.recipes.find((recipe, index) =>{
+      //           return index === this.id;
+      //         });
+      //       })).subscribe(recipe => {
+      //         this.recipe = recipe;
+      //       });
 
-          }
-        );
+      //     }
+      //   );
+          this.route.params
+            .pipe(
+              map(params => {
+                return +params['id'];
+              }),
+              switchMap(id => {
+                this.id = id;
+                return this.store.select('recipes');
+              }),
+              map(recipesState => {
+                return recipesState.recipes.find((recipe, index) => {
+                  return index === this.id;
+                });
+              })
+            )
+            .subscribe(recipe => {
+              this.recipe = recipe;
+          });
     }
 
   onAddToShoppingList(){
